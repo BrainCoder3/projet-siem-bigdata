b02305133e1b4a9030b1358e947adc6db20095c2
doubie <soulamanoepro3@gmail.com>
2026-05-13 20:56:03 +0000

    producer.py - envoi OpenSSH et CICIDS dans Kafka

--- producer.py
+++ producer.py
@@ -1,46 +1,28 @@
-import csv
+import pandas as pd
+from kafka import KafkaProducer
 import json
 import time
-from kafka import KafkaProducer
 
-# Configuration dynamique
-KAFKA_SERVER = 'localhost:9092'
-TOPIC_NAME = 'network-traffic'
-DATASET_PATH = 'data/cicids2017_sample.csv'
-
-def json_serializer(data):
-    return json.dumps(data).encode('utf-8')
-
-# Initialisation du Producer Kafka
 producer = KafkaProducer(
-    bootstrap_servers=[KAFKA_SERVER],
-    value_serializer=json_serializer
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-print(f"--- DÉMARRAGE DU PRODUCER SIEM ---")
-print(f"Envoi des données vers le topic: {TOPIC_NAME}")
+# Envoi OpenSSH dans logs-systeme
+print("Envoi logs OpenSSH...")
+df_ssh = pd.read_csv('data/OpenSSH_2k_log_structured.csv')
+for _, row in df_ssh.iterrows():
+    producer.send('logs-systeme', row.to_dict())
+    time.sleep(0.01)
+print(f"{len(df_ssh)} logs SSH envoyés !")
 
-try:
-    with open(DATASET_PATH, mode='r') as csv_file:
-        # DictReader utilise la 1ère ligne du CSV (headers) pour créer les clés JSON
-        reader = csv.DictReader(csv_file)
-        
-        count = 0
-        for row in reader:
-            # Envoi de la ligne de trafic réseau
-            producer.send(TOPIC_NAME, value=row)
-            
-            count += 1
-            # Simulation temps réel : 0.05s de pause entre chaque ligne
-            time.sleep(0.05)
-            
-            if count % 100 == 0:
-                print(f">> {count} lignes de trafic envoyées...")
+# Envoi CICIDS dans flux-reseau
+print("Envoi trafic réseau CICIDS...")
+df_cicids = pd.read_csv('data/cicids2017_sample.csv')
+for _, row in df_cicids.iterrows():
+    producer.send('flux-reseau', row.to_dict())
+    time.sleep(0.01)
+print(f"{len(df_cicids)} flux réseau envoyés !")
 
-except FileNotFoundError:
-    print(f"Erreur : Le fichier {DATASET_PATH} n'existe pas.")
-except Exception as e:
-    print(f"Erreur lors de l'envoi : {e}")
-finally:
-    producer.flush()
-    print("Flux terminé proprement.")
+producer.flush()
+print("Tous les données sont dans Kafka !")
